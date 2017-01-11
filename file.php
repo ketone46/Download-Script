d0c373e0c8f07fd63d3be001aabde22c06acda48
Rishabh Arora <rishabharora46@gmail.com>
2017-01-11 23:42:29 +0530

    times downloaded.

--- file.php
+++ file.php
@@ -17,10 +17,22 @@ $name_old = $data_query['name_old']; //Original File name.
 //Inserting key_id file id and clicks to link table in database.
 $q2 = "INSERT INTO link (key_id, id, clicks) VALUES ('$key_id','$id','$clicks')";
 mysqli_query($link,$q2);
+
+//getting times downloaded.
+$q3 = "SELECT * FROM link WHERE id='$id' AND clicks = '1'";
+$result3 = mysqli_query($link,$q3);
+while (mysqli_fetch_array($result3))
+{
+	$count = $count + 1;
+}
+if ($count="")
+{
+	$count = 0;
+}
+//Checking for errors in any query.
 echo mysqli_error($link);
 echo "Name :" .$name_old."<br>"; //echoing original file name.
 
-echo "Link : <a href = download.php?key_id=".$key_id.">download.php?key_id=".$key_id."</a>" //Echoing unique link valid for single download.
+echo "Link : <a href = download.php?key_id=".$key_id.">download.php?key_id=".$key_id."</a><br>"; //Echoing unique link valid for single download.
+echo "Times Downloaded : ".$count;
 ?>
-
-
