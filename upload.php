cb3a0abf30aded71499db2cbc2403057d18e1f2e
Rishabh Arora <rishabharora46@gmail.com>
2017-01-23 23:17:26 +0530

    extensions

--- upload.php
+++ upload.php
@@ -1,31 +1,42 @@
 <?php
 include 'db.php'; //including database connection.
 
-$id = uniqid(); //file id.
+$ext_array = array("pdf","png"); //Defining compatible extensions using array.
 
 $name_old = $_FILES['upload']['name']; //original name of file.
 
 $name = pathinfo($name_old); //Separating file into name and extension.
 
-$name_new = $name['filename'] . "_" . $id . "." . $name['extension']; //New name with file id attached.
+$ext = $name['extension']; //Storing extension in variable.
 
-$url = "uploads/{$name_new}"; //Setting file directory with name here uploads is directory.
+if (in_array($ext,$ext_array)) //Checking extension of file.
+   {
+	$id = uniqid(); //file id.
 
-$success = move_uploaded_file($_FILES['upload']['tmp_name'],$url); //moving file to uploads directory.
+    $name_new = $name['filename'] . "_" . $id . "." . $name['extension']; //New name with file id attached.
 
-//Checking upload status.
-if (!$success) 
-    { 
-        echo "<p>Upload Error. <a href = index.php>retry</a>.</p>";
-        exit;
-    }
-else 
-    {
-		//File Link.
-	    echo "<p>File ".$name_old." Uploaded Successdully.<br>link:<a href=file.php?id=".$id.">file.php?id=".$id."</a></p>";
-		//Inserting original name, file id and directory location of file in database.
-		$sql = "INSERT INTO file (name_old, id, url) VALUES ('$name_old','$id','$url')";
-        mysqli_query($link, $sql);
-        mysqli_close($link);
-    }	
+    $url = "uploads/{$name_new}"; //Setting file directory with name here uploads is directory.
+
+    $success = move_uploaded_file($_FILES['upload']['tmp_name'],$url); //moving file to uploads directory.
+
+    //Checking upload status.
+    if (!$success) 
+       { 
+         echo "<p>Upload Error. <a href = index.php>retry</a>.</p>";
+         exit;
+       }
+    else 
+       {
+		 //File Link.
+	     echo "<p>File ".$name_old." Uploaded Successdully.<br>link:<a href=file.php?id=".$id.">file.php?id=".$id."</a></p>";
+		 //Inserting original name, file id and directory location of file in database.
+		 $sql = "INSERT INTO file (name_old, id, url) VALUES ('$name_old','$id','$url')";
+         mysqli_query($link, $sql);
+         mysqli_close($link);
+       }	
+   }
+else
+   {
+	echo "File type not supported"; //If extension is not found in array.
+   }
 ?>
